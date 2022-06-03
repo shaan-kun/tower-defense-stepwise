7ff171ad051ff05c2c957fcb6cb4e71477595ef3
PokemonMagistr <106740187+PokemonMagistr@users.noreply.github.com>
2022-06-03 21:42:21 +0400

    Danya-rating

--- HTML/getData.php
+++ HTML/getData.php
@@ -5,7 +5,7 @@ require("connect.php");
 $result = array();
 
 
-$foodResult = $mysqli->query("SELECT * FROM User");
+$foodResult = $mysqli->query("SELECT login, game_count, game_win, game_loss, score FROM user");
 
 while ($food = $foodResult->fetch_array(MYSQLI_ASSOC)) {
 

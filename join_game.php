8e27481076813a5ca771ada3e277204f72549352
Shaan <shaan-kun@yandex.ru>
2022-06-04 07:49:25 +0400

    замена расширения с html на php

--- join_game.php
+++ join_game.php
@@ -9,11 +9,11 @@
     <div class="main">
         <div class="nav">
             <ul id="navbar">
-                <li><a href="singleplayer.html"><br>Одиночная игра</a></li>
-                <li><a href="multiplayer.html"><br>Мультиплеер</a></li>
-                <li><a href="rating.html"><br>Рейтинг</a></li>
-                <li><a href="rules.html"><br>Правила игры</a></li>
-                <li><a href="settings.html"><br>Настройки</a></li>
+                <li><a href="singleplayer.php"><br>Одиночная игра</a></li>
+                <li><a href="multiplayer.php"><br>Мультиплеер</a></li>
+                <li><a href="rating.php"><br>Рейтинг</a></li>
+                <li><a href="rules.php"><br>Правила игры</a></li>
+                <li><a href="settings.php"><br>Настройки</a></li>
                 <li><a href="scripts/sign_out.php"><br>Выход</a></li>
             </ul>
         </div>
--- multiplayer.php
+++ multiplayer.php
@@ -9,19 +9,19 @@
     <div class="main">
         <div class="nav">
             <ul id="navbar">
-                <li><a href="singleplayer.html"><br>Одиночная игра</a></li>
-                <li><a href="multiplayer.html"><br>Мультиплеер</a></li>
-                <li><a href="rating.html"><br>Рейтинг</a></li>
-                <li><a href="rules.html"><br>Правила игры</a></li>
-                <li><a href="settings.html"><br>Настройки</a></li>
+                <li><a href="singleplayer.php"><br>Одиночная игра</a></li>
+                <li><a href="multiplayer.php"><br>Мультиплеер</a></li>
+                <li><a href="rating.php"><br>Рейтинг</a></li>
+                <li><a href="rules.php"><br>Правила игры</a></li>
+                <li><a href="settings.php"><br>Настройки</a></li>
                 <li><a href="scripts/sign_out.php"><br>Выход</a></li>
             </ul>
         </div>
         Мультиплеер
         <div class="vertical-menu">
-          <a href="my_games.html">Мои игры</a> 
-          <a href="create_game.html">Создать игру</a>
-          <a href="join_game.html">Присоединиться к игре</a>
+          <a href="my_games.php">Мои игры</a> 
+          <a href="create_game.php">Создать игру</a>
+          <a href="join_game.php">Присоединиться к игре</a>
         </div>
     </div>
 </body>
